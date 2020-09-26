1a6a1e68611f9dc0c7e221f2e9184fd42d6a2382
Alen <relizerel@gmail.com>
2020-09-26 15:20:49 +0300

    Добавление решения

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,40 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String model;
+    private int series;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
+
